5dfcb88ae482f8436704553df99d6ea0702de294
zacharyrideaux <zacharyrideaux@gmail.com>
2019-10-12 14:49:41 -0700

    Updated hiragana to unicode.

--- mult_choice.py
+++ mult_choice.py
@@ -6,21 +6,20 @@ int_dict = {
     3: 'c',
     4: 'd',
     5: 'e'
-    }
+}
 
 char_dict = {
-    'a': 'あ'
-    'e': 'え'
-    'i': 'い'
-    'o': 'お'
-    'u': 'う'
-    }
+    'a': u'\u3042',
+    'e': u'\u3048',
+    'i': u'\u3044',
+    'o': u'\u304A',
+    'u': u'\u3046',
+}
 
 
 y = random.randint(1, 5)
-print(char_dict.get(int_dict.get(y)))
+print(char_dict.get(str(int_dict.get(y))) + "\n")
 answer_option = [int_dict.get(y)]
-print()
 x = 0
 while x < 2:
     z = random.randint(1,5)
@@ -30,5 +29,3 @@ while x < 2:
 while len(answer_option) > 0:
     z = random.randint(0, len(answer_option) - 1)
     print(answer_option.pop(z))
-    
-        
