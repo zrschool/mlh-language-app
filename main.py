7832de040be30b3ac9fccf1835c0b8a71e9c1c15
zacharyrideaux <zacharyrideaux@gmail.com>
2019-10-12 16:05:47 -0700

    Update main.html to have submit button

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ class MainPage(webapp2.RequestHandler):
             answer_option = [int_dict.get(random_int)]
             answer_options = []
             x = 0
-            while x < 2:
+            while x < 4:
                 z = random.randint(1,5)
                 if answer_option.count(int_dict.get(z)) < 1:
                     answer_option.append(int_dict.get(z))
